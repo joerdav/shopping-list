3f553c5a10e33fbec42743acdfb0f75f8534779f
joerdav <joe.davidson.21111@gmail.com>
2024-03-12 08:41:36 +0000

    regenerate

--- app/static_dev.go
+++ app/static_dev.go
@@ -1,15 +1,16 @@
 //+build dev
 //go:build dev
+// +build dev
 
 package app
 
 import (
-	"os"
 	"net/http"
+	"os"
 )
 
 // This package allows for tailwind to rebuild the css without rebuilding the entire binary.
 
 func public() http.Handler {
-	return http.StripPrefix("/public/", http.FileServerFS(os.DirFS("public")))
+	return http.StripPrefix("/public/", http.FileServerFS(os.DirFS("app/public")))
 }
